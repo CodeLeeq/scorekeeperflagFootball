a3884beae3ce436b54543c7da2e2a7eb2def25b6
Codeleeq <princefemibabs@gmail.com>
2021-03-17 05:38:47 +0100

    I correct the indentation issues and unwanted empty spaces, while adjusting the reset button to flow ith the others.

--- app/src/main/java/com/example/scorekeeperflagfootball/MainActivity.java
+++ app/src/main/java/com/example/scorekeeperflagfootball/MainActivity.java
@@ -17,68 +17,94 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-//Red buttons methods
-
-
+    //Red buttons methods
     //Add six to the initial score and display it.
     public void teamRedAddSix(View view) {
         scoreRedTeam = scoreRedTeam + 6;
         displayRedScore(scoreRedTeam);
     }
 
+    /**
+     * Add two to Red team score and display it.
+     */
     public void teamRedAddTwo(View view) {
         scoreRedTeam = scoreRedTeam + 2;
         displayRedScore(scoreRedTeam);
     }
 
+    /**
+     * Add One to Red team score and display it.
+     */
     public void teamRedAddOne(View view) {
         scoreRedTeam = scoreRedTeam + 1;
         displayRedScore(scoreRedTeam);
     }
 
+    /**
+     * Add three to Red team score as Field Goal and display it.
+     */
     public void teamRedAddFg(View view) {
         scoreRedTeam = scoreRedTeam + 3;
         displayRedScore(scoreRedTeam);
     }
 
+    /**
+     * Add two to Red team score as Safety Goal and display it.
+     */
     public void teamRedAddSg(View view) {
         scoreRedTeam = scoreRedTeam + 2;
         displayRedScore(scoreRedTeam);
-
     }
 
-    //A display score for Red team
+    //A display method for Red team
     public void displayRedScore(int score) {
         TextView scoreView = findViewById(R.id.red_score);
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * Add one to Yellow team score and display it.
+     */
     public void teamYellowAddOne(View view) {
         scoreYellowTeam = scoreYellowTeam + 1;
         displayYellowScore(scoreYellowTeam);
     }
 
+    /**
+     * Add 2 to Yellow team score and display it.
+     */
     public void teamYellowAddTwo(View view) {
         scoreYellowTeam = scoreYellowTeam + 2;
         displayYellowScore(scoreYellowTeam);
     }
 
+    /**
+     * Add three to Yellow team score and display it.
+     */
     public void teamYellowAddFg(View view) {
         scoreYellowTeam = scoreYellowTeam + 3;
         displayYellowScore(scoreYellowTeam);
     }
 
+    /**
+     * Add two to Yellow team score as safety goal
+     */
     public void teamYellowAddSg(View view) {
         scoreYellowTeam = scoreYellowTeam + 2;
         displayYellowScore(scoreYellowTeam);
     }
 
+    /**
+     * Add six to Yellow team score and display it.
+     */
     public void teamYellowAddSix(View view) {
         scoreYellowTeam = scoreYellowTeam + 6;
         displayYellowScore(scoreYellowTeam);
     }
 
+    /**
+     * Reset all score to Zero and display it.
+     */
     public void ResetAllScore(View view) {
         scoreRedTeam = 0;
         scoreYellowTeam = 0;
@@ -86,6 +112,9 @@ public class MainActivity extends AppCompatActivity {
         displayRedScore(scoreRedTeam);
     }
 
+    /**
+     * Display score for Yellow team and display it.
+     */
     //A display score for Yellow team
     public void displayYellowScore(int score) {
         TextView scoreView = findViewById(R.id.yellow_score);
